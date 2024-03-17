69ae3704057f04903928ea4d840e7a38b7b1e9ac
Hassah <roxirose03@gmail.com>
2024-03-17 02:11:31 -0700

    feat: commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+        Number: 1,
+        question: "What is the full form of CSS",
+        answer:"B. Cascading Style Sheets",
+        options:[
+            "A. Casecading Styles Sheet",
+            "B. Cascading Style Sheets",
+            "C. Casacoding Style Sheets",
+            "D. None of the above",
+        ]
+    },
+    {
+        Number: 2,
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer:"A.  <script>",
+        options:[
+            "A. <script>",
+            "B. <javascript>",
+            "C. <scripting>",
+            "D. <js>",
+       ]
+    },
+    {
+        Number: 3,
+        question: "What is the first mechanical computer designed by Charles Babbage called?",
+        answer:"D. Analytial Engine",
+        options:[
+            "A. Abacus",
+            "B. Calculator",
+            "C. Processor",
+            "D. Analytial Engine",
+        ]
+    },
+    {
+        Number: 4,
+        question: "Which HTML element is used to reference an external JavaScript file?",
+        answer:"A. src",
+        options:[
+            "A. src",
+            "B. href",
+            "C. type",
+            "D. rel",
+        ]
+    },
+    {
+        Number: 5,
+        question: "What type of device is computer monitor?",
+        answer:"B. Output",
+        options:[
+            "A. Input",
+            "B. Output",
+            "C. Software",
+            "D. Storage",
+        ]
+    },
+]
--- scripts.js
+++ scripts.js
@@ -1,18 +1,61 @@
-let count = 0
+//document.addEventListener('DOMContentLoaded', function() {
 
-/**
- * Creating the increment function
- */
-const increment = () => {
-  count += 1
-  document.getElementById('h1').textContent = count
+  const startbtn = document.querySelector('.Start');
+  const guide = document.querySelector('.Guide');
+  const exitbtn = document.querySelector('.btn');
+  const main = document.querySelector('.main');
+  const continuebtn= document.querySelector('.continue');
+  const quizSection= document.querySelector('.quiz-section');
+  const quizBox= document.querySelector('.quiz-box');
+
+startbtn.onclick = () => {
+  guide.classList.add('active');
+  main.classList.add('active');
 }
 
-/**
- * Adding the event listener
- */
-document
-  .getElementById('button')
-  .addEventListener('click', () => increment())
+exitbtn.onclick = () => {
+  guide.classList.remove('active');
+  main.classList.remove('active');
+}
 
+continuebtn.onclick = () => {
+  quizSection.classList.add('active');
+  guide.classList.remove('active');
+  main.classList.remove('active');
+  quizBox.classList.add('active');
 
+  showquestions(0);
+
+}
+//});
+
+let countQuestion = 0;
+
+const nextBtn = document.querySelector('.nextbutton');
+
+nextBtn.onclick = () => {
+  if (countQuestion < questions.length - 1) {
+    countQuestion++;
+    showquestions(countQuestion);
+  }
+
+  else {
+    console.log('done');
+  }
+  
+}
+
+const choiceList = document.querySelector('.choice-list');
+
+// getting questions and choices from array
+function showquestions(index) {
+  const questionText = document.querySelector('.question');
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag =`<div class="choice"><span>${questions[index].options[0]}</span></div>
+  <div class="choice"><span>${questions[index].options[1]}</span></div>
+  <div class="choice"><span>${questions[index].options[2]}</span></div>
+  <div class="choice"><span>${questions[index].options[3]}</span></div>`;
+
+  choiceList.innerHTML = optionTag;
+}
